b967142a3c44e8ba765aacda08e42c45249a9b99
Twillij <silentwill29@gmail.com>
2021-03-14 19:08:04 +1100

    +: Delegates for character attribute changes

--- Source/FAETH/CharacterAttributeSet.h
+++ Source/FAETH/CharacterAttributeSet.h
@@ -7,6 +7,11 @@
 /**
  * 
  */
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnHealthChangeDelegate, float, Health, float, MaxHealth);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnManaChangeDelegate, float, Mana, float, MaxMana);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnStaminaChangeDelegate, float, Stamina, float, MaxStamina);
+
 UCLASS()
 class FAETH_API UCharacterAttributeSet : public UAttributeSet
 {
@@ -15,9 +20,39 @@ class FAETH_API UCharacterAttributeSet : public UAttributeSet
 public:
 	UCharacterAttributeSet();
 
+	FOnHealthChangeDelegate OnHealthChange;
+	FOnManaChangeDelegate OnManaChange;
+	FOnStaminaChangeDelegate OnStaminaChange;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayAttributeData Health;
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	FGameplayAttributeData MaxHealth;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	FGameplayAttributeData Health;
+	FGameplayAttributeData Mana;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayAttributeData MaxMana;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayAttributeData Stamina;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayAttributeData MaxStamina;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayAttributeData Strength;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayAttributeData Defense;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayAttributeData Magic;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayAttributeData MagicDefense;
+
+	void PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data) override;
 };
