5f4eaff15cbc1da5441be7969dc247fbcccccf89
Twillij <silentwill29@gmail.com>
2021-04-23 18:17:15 +1000

    +: Equipment attribute set

---
+++ Source/FAETH/EquipmentAttributeSet.cpp
@@ -0,0 +1,11 @@
+#include "EquipmentAttributeSet.h"
+
+UEquipmentAttributeSet::UEquipmentAttributeSet()
+{
+	Health = 0;
+	Mana = 0;
+	Stamina = 0;
+	Stagger = 0;
+	Attack = 0;
+	Defense = 0;
+}
---
+++ Source/FAETH/EquipmentAttributeSet.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "EquipmentAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FAETH_API UEquipmentAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+public:
+	UEquipmentAttributeSet();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayAttributeData Health;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayAttributeData Mana;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayAttributeData Stamina;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayAttributeData Stagger;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayAttributeData Attack;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayAttributeData Defense;
+};
