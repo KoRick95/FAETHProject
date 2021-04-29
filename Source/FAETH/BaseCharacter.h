e277ad07ffb427a92f6bedb55f65730f7023108f
Twillij <silentwill29@gmail.com>
2021-04-30 01:44:55 +1000

    ~: Refactored character attribute set

--- Source/FAETH/BaseCharacter.h
+++ Source/FAETH/BaseCharacter.h
@@ -3,7 +3,6 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "AbilitySystemInterface.h"
-#include "AbilitySystemComponent.h"
 #include "CharacterAttributeSet.h"
 #include "BaseCharacter.generated.h"
 
