1267e9640844731db6e975cbe3083cacf43a281b
william.tjang <silentwill29@gmail.com>
2022-01-17 23:14:37 +1100

    Added more functions in the function lib

--- Source/FAETH/FaethFunctionLibrary.h
+++ Source/FAETH/FaethFunctionLibrary.h
@@ -4,7 +4,10 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "FaethFunctionLibrary.generated.h"
 
+class UFaethGameInstance;
+class UQuest;
 class UQuestManager;
+class UQuestObjective;
 
 UCLASS()
 class FAETH_API UFaethFunctionLibrary : public UBlueprintFunctionLibrary
@@ -13,5 +16,20 @@ class FAETH_API UFaethFunctionLibrary : public UBlueprintFunctionLibrary
 
 public:
 	UFUNCTION(BlueprintPure, meta = (WorldContext = "WorldContextObject"))
+	static UFaethGameInstance* GetFaethGameInstance(const UObject* WorldContextObject);
+
+	UFUNCTION(BlueprintPure, meta = (WorldContext = "WorldContextObject"))
 	static UQuestManager* GetQuestManager(const UObject* WorldContextObject);
+
+	UFUNCTION(BlueprintPure, Category = "Quest")
+	static UQuest* GetQuestByClass(const TArray<UQuest*>& QuestArray, TSubclassOf<UQuest> QuestClass);
+
+	UFUNCTION(BlueprintPure, Category = "Quest")
+	static UQuest* GetQuestByID(const TArray<UQuest*>& QuestArray, FName QuestID);
+
+	UFUNCTION(BlueprintPure, Category = "Quest")
+	static UQuestObjective* GetObjectiveByClass(const TArray<UQuestObjective*>& ObjectiveArray, TSubclassOf<UQuestObjective> ObjectiveClass);
+
+	UFUNCTION(BlueprintPure, Category = "Quest")
+	static UQuestObjective* GetObjectiveByID(const TArray<UQuestObjective*>& ObjectiveArray, FName ObjectiveID);
 };
