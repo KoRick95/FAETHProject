9f0847075f9712801bde5ccf808213ebdc6e4138
Will <silentwill29@gmail.com>
2022-06-06 00:17:05 +1000

    Added core skill system classes

---
+++ Source/FAETH/SkillManager.cpp
@@ -0,0 +1,2 @@
+#include "SkillManager.h"
+
---
+++ Source/FAETH/SkillNode.cpp
@@ -0,0 +1,8 @@
+#include "SkillNode.h"
+#include "SkillNodeLink.h"
+
+USkillNodeLink* USkillNode::CreateNodeLinkTo(USkillNode* OtherNode)
+{
+	
+	return nullptr;
+}
