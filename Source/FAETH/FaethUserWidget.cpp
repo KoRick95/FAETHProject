3393d747964b0e7213487b1456c80bad38e54bef
Will <silentwill29@gmail.com>
2022-08-07 18:51:56 +1000

    WIP basic widget class

--- Source/FAETH/FaethUserWidget.cpp
+++ Source/FAETH/FaethUserWidget.cpp
@@ -1,1 +1,61 @@
 #include "FaethUserWidget.h"
+
+void UFaethUserWidget::NavigateByIndex(int Index)
+{
+	if (Index >= 0 && Index < NavigableWidgets.Num())
+	{
+		UpdateNavigator(NavigableWidgets[Index], Index);
+	}
+	else if (Index < 0)
+	{
+		NavigateStart();
+	}
+	else if (Index >= NavigableWidgets.Num())
+	{
+		NavigateEnd();
+	}
+}
+
+void UFaethUserWidget::NagivateNext()
+{
+	int NewIndex = GetCurrentIndex() + 1;
+
+	if (NewIndex < NavigableWidgets.Num())
+	{
+		UpdateNavigator(NavigableWidgets[NewIndex], NewIndex);
+	}
+	else if (bWrapAround)
+	{
+		NavigateStart();
+	}
+}
+
+void UFaethUserWidget::NavigatePrev()
+{
+	int NewIndex = GetCurrentIndex() - 1;
+
+	if (NewIndex >= 0)
+	{
+		UpdateNavigator(NavigableWidgets[NewIndex], NewIndex);
+	}
+	else if (bWrapAround)
+	{
+		NavigateEnd();
+	}
+}
+
+void UFaethUserWidget::NavigateStart()
+{
+	UpdateNavigator(NavigableWidgets[0], 0);
+}
+
+void UFaethUserWidget::NavigateEnd()
+{
+	UpdateNavigator(NavigableWidgets[NavigableWidgets.Num()], NavigableWidgets.Num());
+}
+
+void UFaethUserWidget::UpdateNavigator(UWidget* NewWidget, int NewIndex)
+{
+	WidgetNavigator.Widget = NewWidget;
+	WidgetNavigator.Index = NewIndex;
+}
--- Source/FAETH/FaethUserWidget.h
+++ Source/FAETH/FaethUserWidget.h
@@ -13,10 +13,10 @@ public:
 	FWidgetNavigator() {}
 	FWidgetNavigator(UWidget* InWidget, int InIndex) : Widget(InWidget), Index(InIndex) {}
 
-	UPROPERTY(BlueprintReadOnly)
+	UPROPERTY(BlueprintReadWrite)
 	UWidget* Widget;
 
-	UPROPERTY(BlueprintReadOnly)
+	UPROPERTY(BlueprintReadWrite)
 	int Index;
 };
 
@@ -26,6 +26,11 @@ class FAETH_API UFaethUserWidget : public UUserWidget
 	GENERATED_BODY()
 	
 public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Navigation")
+	// If set to true, the navigation will wrap around the array.
+	// I.e. Navigating next from the end of the array will go back to the start and vice versa.
+	bool bWrapAround = false;
+	
 	UPROPERTY(BlueprintReadWrite, Category = "Navigation")
 	TArray<UWidget*> NavigableWidgets;
 
@@ -33,5 +38,27 @@ public:
 	FWidgetNavigator WidgetNavigator;
 
 public:
-	
+	UFUNCTION(BlueprintCallable, Category = "Navigation")
+	int GetCurrentIndex() { return WidgetNavigator.Index; }
+
+	UFUNCTION(BlueprintCallable, Category = "Navigation")
+	UWidget* GetCurrentWidget() { return WidgetNavigator.Widget; }
+
+	UFUNCTION(BlueprintCallable, Category = "Navigation")
+	virtual void NavigateByIndex(int Index);
+
+	UFUNCTION(BlueprintCallable, Category = "Navigation")
+	virtual void NagivateNext();
+
+	UFUNCTION(BlueprintCallable, Category = "Navigation")
+	virtual void NavigatePrev();
+
+	UFUNCTION(BlueprintCallable, Category = "Navigation")
+	virtual void NavigateStart();
+
+	UFUNCTION(BlueprintCallable, Category = "Navigation")
+	virtual void NavigateEnd();
+
+protected:
+	void UpdateNavigator(UWidget* NewWidget, int NewIndex);
 };
