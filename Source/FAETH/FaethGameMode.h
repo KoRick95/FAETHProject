620f23fc34016dbb00e7b7304a8ac30e2a78021f
Will <silentwill29@gmail.com>
2022-09-05 03:26:43 +1000

    Added empty GameMode class

---
+++ Source/FAETH/FaethGameMode.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FaethGameMode.generated.h"
+
+UCLASS()
+class FAETH_API AFaethGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	// To do: OnPlayerLoaded
+};
