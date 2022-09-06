9ebb525629be11cb9bb6aa544b3cf69c9d292140
Will <silentwill29@gmail.com>
2022-09-07 01:53:16 +1000

    Created empty PlayerState class

---
+++ Source/FAETH/FaethPlayerState.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "FaethPlayerState.generated.h"
+
+UCLASS()
+class FAETH_API AFaethPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+public:
+	// To do: Store player skills here!
+};
